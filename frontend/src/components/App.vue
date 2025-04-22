e52f8ebedf3f422ae0ee2c53744f3d2a47897d4f
jdColonia <JuanDavidColoniaAldana@gmail.com>
2025-04-21 21:30:25 -0500

    cambio a negro del color

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -16,7 +16,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #caf;
+  color: #111;
   margin-top: 60px;
 }
 </style>

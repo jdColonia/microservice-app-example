a92b85fcbeedc0eb702703ef53a10e6696cac23f
jdColonia <JuanDavidColoniaAldana@gmail.com>
2025-04-21 22:19:05 -0500

    update

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -16,7 +16,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #111;
+  color: #ff5733;
   margin-top: 60px;
 }
 </style>
